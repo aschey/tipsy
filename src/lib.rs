225d5391cf8e360b9b66e71b2c97d27e61104ec5
Austin Schey <aschey13@gmail.com>
2025-04-10 22:36:13 -0700

    chore: remove deprecated API usage in tests (#11)

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,6 @@
 
 #![deny(missing_docs)]
 #![forbid(clippy::unwrap_used)]
-#![deny(rustdoc::broken_intra_doc_links)]
-#![warn(clippy::semicolon_if_nothing_returned)]
-#![warn(clippy::doc_markdown)]
-#![warn(clippy::default_trait_access)]
-#![warn(clippy::ignored_unit_patterns)]
-#![warn(clippy::semicolon_if_nothing_returned)]
-#![warn(clippy::missing_fields_in_debug)]
-#![warn(clippy::use_self)]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc = include_str!("../README.md")]
 
